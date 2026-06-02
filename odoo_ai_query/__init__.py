7270c95c54853a0ca6ed7f807d686bd00b9bb41d
Sebin Thomas <sebin.thomas@beo.in>
2026-06-02 11:52:48 +0530

    feat: restructure odoo_ai_query module and add query execution controller

--- odoo_ai_query/__init__.py
+++ odoo_ai_query/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ['controllers', 'models']
+# -*- coding: utf-8 -*-
+"""Top-level package for odoo_ai_query module."""
 
 from . import controllers
 from . import models
--- query-ai/odoo_ai_query/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Top-level package for odoo_ai_query module."""
-
-from . import controllers
-from . import models
